5425f398c5f1f3fc2446b5d606e73857e2e27c7a
MariaMantusova <maria.mantusova@mail.ru>
2023-10-23 18:44:20 +0300

    feat: add gitignore

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 window.addEventListener("scroll", e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+
+ScrollSmoother.create({
+    wrapper: ".wrapper",
+    content: ".container"
 })
